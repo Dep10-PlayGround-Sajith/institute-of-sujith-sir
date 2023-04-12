2c490bd9b050692c0c482f76f6ccc15788a966e2
nishan akalanka <hdnakalanka@gmail.com>
2023-04-12 09:14:27 +0530

    PaymentScene-GUI-Created

--- src/main/java/lk/ijse/dep10/app/control/PaymentSceneController.java
+++ src/main/java/lk/ijse/dep10/app/control/PaymentSceneController.java
@@ -1,4 +1,43 @@
 package lk.ijse.dep10.app.control;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+
 public class PaymentSceneController {
+
+    public Button btnAdd;
+    public Button btnCancel;
+    public Button btnPay;
+    public Button btnRemove;
+    public ComboBox<?> cmbClass;
+    public ImageView imgProfPic;
+    public Label label;
+    public TableView<?> tblClassFeePay;
+    public TableView<?> tblPaymentDetails;
+    public TextField txtAdmission;
+    public TextField txtID;
+    public TableColumn colFromMonth;
+    public TableColumn colToMonth;
+    public TableColumn colTotalFee;
+    public TextField txtTotal;
+
+    public void btnAddOnAction(ActionEvent event) {
+
+    }
+    public void btnCancelOnAction(ActionEvent event) {
+
+    }
+    public void btnPayOnAction(ActionEvent event) {
+
+    }
+    public void btnRemoveOnAction(ActionEvent event) {
+
+    }
+    public void txtIDOnKeyReleased(KeyEvent event) {
+
+    }
+
 }
